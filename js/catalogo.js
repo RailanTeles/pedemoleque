8a8d15994e8d10ad48c13c568766e5aab6e9b572
RailanTeles <135670809+RailanTeles@users.noreply.github.com>
2024-11-10 19:40:53 -0300

    Página catalogo quase pronta

--- js/catalogo.js
+++ js/catalogo.js
@@ -50,3 +50,63 @@ seletores.forEach((seletor, index) => {
         });
     });
 });
+
+
+
+// Carrossel
+
+// Funções
+function setadireita(count, modelo){
+    if(count != 4){
+        count = count + 1;
+        modelo.style.marginLeft = `-${100 * count}%`;
+    }else{
+        count = 0;
+        modelo.style.marginLeft = `-${100 * count}%`;
+    }
+    return count;
+}
+
+function setaesquerda(count, modelo){
+    if(count != 0){
+        count = count - 1;
+        modelo.style.marginLeft = `-${100 * count}%`;
+    }else{
+        count = 4;
+        modelo.style.marginLeft = `-${100 * count}%`;
+    }
+    return count;
+}
+
+// Feminino PRAIA
+const setaDirFemPraia = document.querySelector('#setadirpraiafem');
+const setaEsqrFemPraia = document.querySelector('#setaesqpraiafem');
+const modeloFemPraia = document.querySelector('.modelofempraia1');
+
+let countFemPraia = 0
+
+setaDirFemPraia.addEventListener('click', function(){
+    countFemPraia = setadireita(countFemPraia, modeloFemPraia);
+})
+
+
+setaEsqrFemPraia.addEventListener('click', function(){
+    countFemPraia = setaesquerda(countFemPraia, modeloFemPraia);
+})
+
+//  Feminino Roupas
+
+const setaDirFemRoupa = document.querySelector('#setadirfemroupa');
+const setaEsqrFemRoupa = document.querySelector('#setaesqfemroupa');
+const modeloFemRoupa = document.querySelector('.modelofemroupa1');
+
+let countFemRoupa = 0
+
+setaDirFemRoupa.addEventListener('click', function(){
+    countFemRoupa = setadireita(countFemRoupa, modeloFemRoupa);
+})
+
+
+setaEsqrFemRoupa.addEventListener('click', function(){
+    countFemRoupa = setaesquerda(countFemRoupa, modeloFemRoupa);
+})
